6e4739f7eec424ef400f6b0d4ed584a682a7d34c
sh4dqwx <bart.karolczak@gmail.com>
2025-07-11 17:38:52 +0200

    Added yay setup to init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 
+# Setting up Yay
+sudo pacman -S git base-devel
+git clone https://aur.archlinux.org/yay.git ~/dev/tools/yay
+cd ~/dev/tools/yay
+makepkg -si
+
 # Setting up Zsh
 sudo pacman -S zsh
 chsh -p /usr/bin/zsh
-
+# Setting up Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir -p ~/dev/tools
 mv ~/.oh-my-zsh ~/dev/tools/oh-my-zsh
