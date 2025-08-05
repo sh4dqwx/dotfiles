b0a50bd931218abedf3ae2361c610d574308ce63
sh4dqwx <bart.karolczak@gmail.com>
2025-08-05 19:14:10 +0200

    fixes

--- init.sh
+++ init.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/bash
 
+# Make dev directory
+mkdir -p ~/dev/tools
+
 # Setting up Yay
-sudo pacman -S git base-devel
+sudo pacman -S go
 git clone https://aur.archlinux.org/yay.git ~/dev/tools/yay
 cd ~/dev/tools/yay
 makepkg -si
 
 # Setting up Zsh
-sudo pacman -S zsh
+sudo pacman -S zsh --noconfirm
 chsh -s /usr/bin/zsh
 # Setting up Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir -p ~/dev/tools
 mv ~/.oh-my-zsh ~/dev/tools/oh-my-zsh
 
 # Setting up Neovim
-sudo pacman -S neovim python gcc tmux ripgrep fd
+sudo pacman -S python gcc tmux ripgrep fd --noconfirm
 yay -S python312
 
 cd ~/.dotfiles
