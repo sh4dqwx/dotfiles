52f826da8cdd274fe086d53e5e3d27011e46723c
sh4dqwx <bart.karolczak@gmail.com>
2025-08-05 19:19:08 +0200

    added installing terraform

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 # Make dev directory
 mkdir -p ~/dev/tools
+mkdir ~/dev/private
 
 # Setting up Yay
 sudo pacman -S go
@@ -19,6 +20,9 @@ mv ~/.oh-my-zsh ~/dev/tools/oh-my-zsh
 # Setting up Neovim
 sudo pacman -S python gcc tmux ripgrep fd --noconfirm
 yay -S python312
+
+# Other (add somewhere later)
+sudo pacman -S terraform --noconfirm
 
 cd ~/.dotfiles
 stow .
