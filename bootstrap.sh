24477fb25194581bf7a49c4580a5d704ff64af2e
sh4dqwx <bart.karolczak@gmail.com>
2025-08-09 12:18:58 +0200

    changed init.sh to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/bash
 
-# Make dev directory
-mkdir -p ~/dev/tools
-mkdir ~/dev/private
+# Install all necessary packages from pacman
+sudo pacman -S go zsh python gcc tmux ripgrep fd terraform --noconfirm
+
+# Create directories for dev projects
+mkdir -p ~/dev/tools ~/dev/private
 
 # Setting up Yay
-sudo pacman -S go
 git clone https://aur.archlinux.org/yay.git ~/dev/tools/yay
 cd ~/dev/tools/yay
 makepkg -si
+cd ~
 
-# Setting up Zsh
-sudo pacman -S zsh --noconfirm
+# Install all necessary packages from yay
+yay -S python312 --noconfirm
+
+# Setting up Zsh and Oh My Zsh
 chsh -s /usr/bin/zsh
-# Setting up Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv ~/.oh-my-zsh ~/dev/tools/oh-my-zsh
-
-# Setting up Neovim
-sudo pacman -S python gcc tmux ripgrep fd --noconfirm
-yay -S python312
-
-# Other (add somewhere later)
-sudo pacman -S terraform --noconfirm
 
 cd ~/.dotfiles
 stow .
