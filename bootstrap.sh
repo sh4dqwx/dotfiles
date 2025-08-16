f8a9998fd728f01a929b32ce75fe5e964905b917
sh4dqwx <bart.karolczak@gmail.com>
2025-08-16 16:47:42 +0200

    changed installing oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/bash
 
-# Install all necessary packages from pacman
-sudo pacman -S go zsh python gcc tmux ripgrep fd terraform --noconfirm
-
 # Create directories for dev projects
 mkdir -p $HOME/dev/tools $HOME/dev/private
 
-# Setting up Yay
-# git clone https://aur.archlinux.org/yay.git ~/dev/tools/yay
-# cd $HOME/dev/tools/yay
-# makepkg -si
-# cd $HOME
+# Install Yay
+sudo pacman -S go --noconfirm
+git clone https://aur.archlinux.org/yay.git ~/dev/tools/yay
+cd $HOME/dev/tools/yay
+makepkg -si
+cd $HOME
 
-# Install all necessary packages from yay
-# yay -S python312 --noconfirm
+# Install all necessary packages from pacman
+yay -S go zsh oh-my-zsh-git python gcc tmux ripgrep fd terraform --noconfirm
 
-# Setting up Zsh and Oh My Zsh
-# chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# rm $HOME/.zshrc.pre-oh-my-zsh
-mv $HOME/.oh-my-zsh $HOME/dev/tools/oh-my-zsh
+# Setting up Zsh as default shell
+chsh -s /usr/bin/zsh
 
 cd $HOME/.dotfiles
 stow --adopt .
