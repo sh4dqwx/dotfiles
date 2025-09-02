664227941a57fa009f6293e45ead5082ca725de6
sh4dqwx <bart.karolczak@gmail.com>
2025-09-02 19:25:27 +0200

    added git to init_wsl

--- init_wsl.sh
+++ init_wsl.sh
@@ -45,13 +45,22 @@ main() {
 	locale-gen
 	
 	pacman -Syu --noconfirm
-	pacman -S base-devel fd gcc go imagemagick less neovim openssh python ripgrep stow sudo terraform tmux wl-clipboard zsh --noconfirm
+	pacman -S base-devel fd gcc git go imagemagick less neovim openssh python ripgrep stow sudo terraform tmux wl-clipboard zsh --noconfirm
 
 	set_password "root" $ROOT_PASSWORD
 	create_user $USERNAME $PASSWORD
 
 	sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //' /etc/sudoers
 	echo -e "\n[user]\ndefault=$USERNAME" >> /etc/wsl.conf
+
+	sudo -u $USERNAME bash <<EOF
+	git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+	git config --global credential.https://dev.azure.com.useHttpPath true
+	git config --global init.defaultBranch main
+	git config --global pull.rebase false
+	git config --global core.editor "nvim"
+	git clone https://github.com/sh4dqwx/dotfiles.git ~/.dotfiles
+	EOF
 }
 
 main $@
